9a3b3e80862ca964901ca6a9515fad405511c12c
wstaempfli <stampfliwanja4@gmail.com>
2025-04-13 20:08:23 +0200

    T3 commit

--- 01_baby_steps/myserver/server.py
+++ 01_baby_steps/myserver/server.py
@@ -3,6 +3,13 @@ import http.server
 import socketserver
 import argparse
 
+class MyHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(b"Hello Tarrif$")
+
 def main():
     parser = argparse.ArgumentParser(description="Simple HTTP server")
     parser.add_argument("port", nargs="?", type=int, default=8000)
@@ -10,8 +17,10 @@ def main():
 
     port = args.port
 
-    ## TODO-03:  
-
+    # Server setup
+    with socketserver.TCPServer(("0.0.0.0", port), MyHandler) as httpd:
+        print(f"Serving at 0.0.0.0:{port}")
+        httpd.serve_forever()
 
 if __name__ == "__main__":
     main()
